bf6be265b5243aa6d978e47931241e70183a0da6
VLautaroUTN <utnlautaro@gmail.com>
2023-05-28 00:12:51 -0300

    Se separara  cada linea en  lista por str split

--- prueba_1.py
+++ prueba_1.py
@@ -1,12 +1,41 @@
+file = open('test.ngc', 'r')
+
 def leer():
     for x in range(4):
         contents = file.readline()
         print(contents)
+        file.close()
 
 def tipo():
     contents = file.readline()
     print(type(contents))
+    file.close()
 
-file = open('test.ngc', 'r')
-tipo() #devuelve string :D
-file.close()
+
+
+"""------------------------------------------------------------------
+Método string split
+
+El split() function, es usado usualmente para dividir strings.
+El  método split()
+
+Template: string.split(separador, maxdivision)
+
+separador: El delimitador del string. Tú divides el string basado en estos caracteres. Por ejemplo (" "), (":"), (";"), etc.
+
+maxdivision: El número de veces a dividir el string basados en el separador. Si, no especificamos éste número o ponemos -1, el string tomara todas las occurrencias del separator.
+
+Este metodo retorna una lista de substrings delimitados por el separator.
+Ejemplos
+
+String dividido con espacio en blanco " "
+
+string = "freeCodeCamp is fun."
+print(string.split(" "))
+
+Delimitador (" ") - Python3.7
+
+Resultado:
+
+['freeCodeCamp', 'is', 'fun.']
+"""
