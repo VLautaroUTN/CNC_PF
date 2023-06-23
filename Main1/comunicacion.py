a7628af557a4ce603c7e27ef8f4b40da570947eb
VLautaroUTN <utnlautaro@gmail.com>
2023-06-22 23:08:21 -0300

    --

--- Main1/comunicacion.py
+++ Main1/comunicacion.py
@@ -1,25 +1,28 @@
 import serial, time
 
-coorfile = open('coordenadas.ngc', 'r')   
-mensaje = ''
-arduino = serial.Serial('COM4', 9600)
-time.sleep(2)
+
+def comunicacion(archivo, puerto = "COM4"): #Recibe como datos el nombre 
+    # del archivo y el puerto de conexion (VALOR POR DEFECTO COM4)
+    coorfile = open(archivo, 'r')   
+    mensaje = ''
+    arduino = serial.Serial(puerto, 9600)
+    time.sleep(2)
 
 
-for lineas in coorfile:
-    linea = lineas.split(' ')
-    if linea[0] == 'G01':
-        mensaje = '1,'
-    else:
-        mensaje = '0,'
+    for lineas in coorfile:
+        linea = lineas.split(' ')
+        if linea[0] == 'G01':
+            mensaje = '1,'
+        else:
+            mensaje = '0,'
 
-    mensaje = mensaje + ',' + linea[1] + ',' + linea[2]
+        mensaje = mensaje + ',' + linea[1] + ',' + linea[2]
 
-    arduino.write(mensaje.encode('utf-8'))
-    time.sleep(0.5)
-    respuesta = arduino.readline()
-    if respuesta == 1: # Aqui va la comprobacion que la respuesta de arduino sea 1
-        pass
+        arduino.write(mensaje.encode('utf-8'))
+        time.sleep(0.5)
+        respuesta = arduino.readline()
+        if respuesta == 1: # Aqui va la comprobacion que la respuesta de arduino sea 1
+            pass
 
-arduino.close()
-coorfile.close()
+    arduino.close()
+    coorfile.close()
