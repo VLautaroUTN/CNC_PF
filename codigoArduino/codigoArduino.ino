33ccc3feb0315ff4f92d8cfeb95a0f0bb30b0818
VLautaroUTN <utnlautaro@gmail.com>
2023-06-22 22:02:13 -0300

    Casi funciona, falta corregir el valor tomado por orden

--- codigoArduino/codigoArduino.ino
+++ codigoArduino/codigoArduino.ino
@@ -1,25 +1,31 @@
 #include <Stepper.h>
+#include <Servo.h>
 
 const int pasosPorVuelta = 20;
-const velocidadMotores = 250;
+const int velocidadMotores = 250;
 
 //MotorX
-Stepper motorX(pasosPorVuelta, 8,9,10,11);
+Stepper motorX(pasosPorVuelta, 2,3,4,5);
 //Motor Y
-Stepper motorY(pasosPorVuelta, 12,13,14,15);
+Stepper motorY(pasosPorVuelta, 6,7,8,9);
+
+//Servo
+Servo servo;
+const int pinServo = 12;
 
 
 void setup() {
   Serial.begin(9600);
   motorX.setSpeed(velocidadMotores);
   motorY.setSpeed(velocidadMotores);
+  servo.attach(pinServo);
 }
 
 
 void loop() {
   if(Serial.available() > 0){
     String comunicacion = Serial.readStringUntil('\n');
-    String orden = comunicacion.substring(0,1);
+    String orden = comunicacion.substring(1,2);
     String valX = comunicacion.substring(3,9);
     String valY = comunicacion.substring(10);
     
@@ -27,15 +33,15 @@ void loop() {
     int intValX = valX.toInt();
     int intValY =valY.toInt();
 
-    if (orden == 1){
-       servo.write(140);
+    if (ordenint == 1){
+      servo.write(140);
     }
     else{
-        servo.write(120);
+      servo.write(120);
     }
 
     motorX.step(intValX);
-    motorY.setp(intValY);
+    motorY.step(intValY);
 
     Serial.println(1);
   }
