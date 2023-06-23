cbfed4c20f89c7501af12a2cd900ddc7a5ba398a
VLautaroUTN <utnlautaro@gmail.com>
2023-06-22 22:31:46 -0300

    Programa finalizado V1

--- codigoArduino/codigoArduino.ino
+++ codigoArduino/codigoArduino.ino
@@ -3,6 +3,8 @@
 
 const int pasosPorVuelta = 20;
 const int velocidadMotores = 250;
+const int servoArriba = 50;
+const int servoAbajo = 30;
 
 //MotorX
 Stepper motorX(pasosPorVuelta, 2,3,4,5);
@@ -19,27 +21,28 @@ void setup() {
   motorX.setSpeed(velocidadMotores);
   motorY.setSpeed(velocidadMotores);
   servo.attach(pinServo);
+  servo.write(servoArriba);
 }
 
 
 void loop() {
   if(Serial.available() > 0){
     String comunicacion = Serial.readStringUntil('\n');
-    String orden = comunicacion.substring(2,3);
+    String orden = comunicacion.substring(0,1);
     String valX = comunicacion.substring(3,9);
     String valY = comunicacion.substring(10);
     
-    Serial.println("-> " + orden)
+    Serial.println("-> " + orden);
 
     int ordenint = orden.toInt();
     int intValX = valX.toInt();
     int intValY =valY.toInt();
 
     if (ordenint == 1){
-      servo.write(140);
+      servo.write(servoAbajo);
     }
     else{
-      servo.write(120);
+      servo.write(servoArriba);
     }
 
     motorX.step(intValX);
