16e04e5368377ca7f857d7c604ef8e1c56f07e08
VLautaroUTN <utnlautaro@gmail.com>
2023-07-17 23:46:01 -0300

    --

--- TraduccionNGC/DepuradorNGC.py
+++ TraduccionNGC/DepuradorNGC.py
@@ -1,22 +1,3 @@
-"""
-proceso:
-
-proxima tarea #2: Filtrar como se elimino F pero con Z
-asi despues solo tendria que filtrar entre lineas con 3 elementos y con 1
-
-esos dos tipos ->
-G00
-G0x X5.000000 Y5.000000
-
-se guardarian solo los que tengan 3 elementos
-
-El problema es que al final de cada linea agrega un /n
-¿Debo sacar ese /n? ¿Es necesario?
-Objetivo: Crear a partir del archivo .ngc otro archivo
-que solo contenga [bool, int, int ]
-                  [servo, x ,  y  ]
-"""
-
 entrada = open('output_2_0001.ngc', 'r')
 salida = open('coordenadas.ngc', 'w')
 
@@ -25,40 +6,44 @@ def filtrar(lista):
     for x in reversed(lista):
         if 'F' in x:
             lista.remove(x)
-        if 'Z' in x:
+        elif 'Z' in x:
             lista.remove(x)
-    return lista
+        else:
+            pos = lista.index(x)
+            lista[pos] = x.strip('XY\n')
+
+    if lista[0] == "G00":
+        lista[0] = 0
+    else:
+        lista[0] = 1
+    if len(lista) == 3:
+        return lista
+
+# Capaz pueda usar esta misma funcion para filtrar y retornar las coordenadas
 
 
-def buscarCoordenadas(listaContenidoPrima):
-    if len(listaContenidoPrima) ==3:
-        valX = listaContenidoPrima[1]
-        valY = listaContenidoPrima[2]
-        #x = valX[1:9]
-        #y = valY[1:9]
-        valores = [valX[1:7], valY[1:7]]
-        return valores
-
-
-def decodificar2():
+def decodificar():
     for linea in entrada:
-        listaContenido = linea.split(' ')
+        listaContenido = linea.split(' ') #Transformo cada linea en una lista
+
         if listaContenido[0] == "G00" or listaContenido[0] == "G01":
             listaContenidoFiltrada = filtrar(listaContenido)
-            coordenadas = buscarCoordenadas(listaContenidoFiltrada)
-            if coordenadas != None:
-                coordenadas.insert(0, listaContenido[0])
-                print(coordenadas)
-                auxiliar = 0
-                for x in coordenadas:
-                    auxiliar += 1
-                    if auxiliar == 3:
-                        salida.write(x)
+
+            if listaContenidoFiltrada != None:
+                variableAuxiliar = 0
+                for x in listaContenidoFiltrada:
+                    variableAuxiliar += 1
+                    if not variableAuxiliar == 1:
+                        x = float(x)
+                        x = "{:.2f}".format(x)
+                    if variableAuxiliar == 3:
+                        salida.write(str(x) + "_")
                     else:
-                        salida.write(x + ' ')
+                        salida.write(str(x) + "/")
                 salida.write('\n')
 
+
 if __name__ == "__main__":
-    decodificar2()
+    decodificar()
     entrada.close()
     salida.close()
