c7afe1761011d0a64a7ff3fb5e09f76a405a5900
VLautaroUTN <utnlautaro@gmail.com>
2023-07-19 23:26:40 -0300

    --

--- CampoDePruebas/lectorSD.py
+++ CampoDePruebas/lectorSD.py
@@ -1,9 +1,11 @@
 #Este archivo lee las coordenadas y las segmenta para su uso
 #Es de prueba ya que el sketch final debe estar en formato .ino
 lista = []
+datos = []
 
 def setup():
     archivo = open("coordenadas.ngc", 'r')
+    #archivo_progreso = open("progreso.txt, w")
     elemento = ""
     while True:
         letra = archivo.read(1) #Lee de a 1 caracter por vez
@@ -20,9 +22,14 @@ def setup():
 
 
 def loop():
+    progreso = 0
     while True:
-        pass
+        datos = lista[progreso].split("/")
+        print(datos)
+        progreso += 1
+
 
 
 if __name__ == "__main__":
     setup()
+    loop()
