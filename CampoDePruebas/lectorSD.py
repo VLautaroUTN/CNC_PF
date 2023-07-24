2e37bdfa21f16b86256250148f9e887163e2d802
VLautaroUTN <utnlautaro@gmail.com>
2023-07-24 17:18:01 -0300

    asd

--- CampoDePruebas/lectorSD.py
+++ CampoDePruebas/lectorSD.py
@@ -3,15 +3,14 @@
 lista = []
 datos = []
 
+
 def setup():
     archivo = open("coordenadas.ngc", 'r')
-    #archivo_progreso = open("progreso.txt, w")
+    archivo_progreso = open("progreso.txt", 'w')
     elemento = ""
     while True:
         letra = archivo.read(1) #Lee de a 1 caracter por vez
-
         if not letra: break # Se comporta como el archivo.available()
-
         if letra == "_": #  Si la letra es un guion se guarda el string "elemento"
                         # en una lista y posteriormente se reinicia elemento a vacio
             lista.append(elemento)
@@ -24,10 +23,13 @@ def setup():
 def loop():
     progreso = 0
     while True:
-        datos = lista[progreso].split("/")
-        print(datos)
-        progreso += 1
-
+        if progreso < len(lista):
+            datos = lista[progreso].split("/")
+            print("datos")
+            print(datos)
+            progreso += 1
+        else:
+            break
 
 
 if __name__ == "__main__":
