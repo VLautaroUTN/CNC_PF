811ac5d1c5774c1d99b68152d0bffba18716f797
VLautaroUTN <utnlautaro@gmail.com>
2023-08-01 09:49:54 -0300

    --

--- CampoDePruebas/V2LectorSD.ino
+++ CampoDePruebas/V2LectorSD.ino
@@ -7,7 +7,7 @@
 const char* coorFileName = "coordenadas.txt";
 const char* progresoFileName = "progreso.txt";
 int porgreso;
-float* arrayDeValores;
+float arrayDeValores[3];
 File archivo_coordenadas = SD.open(coorFileName, FILE_READ);
 
 int leerProgreso(){
@@ -51,65 +51,33 @@ void iniciarTarjeta(){
     Serial.println("Inicializacion exitosa");
 }
 
-//NO USADA
-int dimensionarListaDeDatos(){ //NO USADA Devuelve la cantidad de valores que tiene coordenadas.txt
-    int longitudDeDatos = 0;
-    archivo_coordenadas = SD.open(coorFileName, FILE_READ);
-    if (archivo_coordenadas){
-        while (archivo_coordenadas.available()){
-            if (archivo_coordenadas.read() == "_"){
-                longitudDeDatos++;
-            }
-        }
-    }
-    archivo_coordenadas.close();
-    return longitudDeDatos;
-}
 
-
-void separarElementosEnLista(){
-    archivo_coordenadas = SD.open(coorFileName, FILE_READ);
-    int contador_de_guiones = 0;
-    int numero_de_dato = 0;
-    int GXY = 0; //Simboliza la posicion del dato 1er 2do o 3er posicion: G = tipo de orden, X = Coordenada x e Y= coordenada y
-    String dato = "";
-    char caracter;
-    if (archivo_coordenadas){ //  0/39.58/143.79_   1/39.51/3.50_
-        while (archivo_coordenadas.available()){
-            caracter = archivo_coordenadas.read();
-            if (caracter == "_"){
-                arrayDeValores[contador_de_guiones][GXY] = dato.toFloat();
-                dato = "";
-                GXY = 0;
-                contador_de_guiones++;
-            }
-            else{
-                if(caracter == "/"){
-
-                    arrayDeValores = new float []
-
-                    arrayDeValores[contador_de_guiones][GXY] = dato.toFloat();
-                    dato = "";
-                    GXY++;
-                }
-                else{dato.concat(caracter);}
-            }
-        }
-    }
-    archivo_coordenadas.close();
-}
-
-
-void leerDatos(File archivo) { 
+void leerDatos(File archivo, int progreso) { 
     while (archivo.available()) { 
         if (archivo) { 
-            String texto = ""; 
+            String texto = "";
+            int valorDeDato = 0;
+            int cantidadGuiones = 0;
             while (archivo.available()) { 
                 char caracter = archivo.read(); 
-                if (caracter == '_') { 
-                    break; // Se encontró la barra baja, se sale del bucle 
-                } 
-                texto += caracter; // Se agrega el carácter al string 
+                if (cantidadGuiones == progreso){
+                    if (caracter == '_') {
+                        arrayDeValores[valorDeDato] = texto.toFloat();
+                        texto = "";
+                        cantidadGuiones += 1;
+                        break
+                    }
+                    if (caracter == '/') {
+                        arrayDeValores[valorDeDato] = texto.toFloat();
+                        valorDeDato += 1;
+                        texto = "";
+                    }
+                    texto += caracter; // Se agrega el carácter al string 
+                } else {
+                    if(caracter == "_"){
+                        cantidadGuiones += 1;
+                    }
+                }
             } 
             archivo.close(); 
 
