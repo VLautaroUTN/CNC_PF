01a9367081910d9214232ff77a8fcba64ed3b56d
VLautaroUTN <utnlautaro@gmail.com>
2023-11-08 20:17:31 -0300

    FUNCIONAAAA

--- Firmata/C#/FirmataC/Program.cs
+++ Firmata/C#/FirmataC/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Solid.Arduino;
+using Solid.Arduino.Firmata;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISerialConnection connection = GetConnection();
+
+            if (connection != null)
+                using (var session = new ArduinoSession(connection))
+                    PerformBasicTest(session);
+
+            Console.WriteLine("Press a key");
+            Console.ReadKey(true);
+        }
+
+        private static ISerialConnection GetConnection()
+        {
+            Console.WriteLine("Searching Arduino connection...");
+            ISerialConnection connection = EnhancedSerialConnection.Find();
+
+            if (connection == null)
+                Console.WriteLine("No connection found. Make shure your Arduino board is attached to a USB port.");
+            else
+                Console.WriteLine($"Connected to port {connection.PortName} at {connection.BaudRate} baud.");
+
+            return connection;
+        }
+
+        private static void PerformBasicTest(IFirmataProtocol session)
+        {
+            var firmware = session.GetFirmware();
+            Console.WriteLine($"Firmware: {firmware.Name} version {firmware.MajorVersion}.{firmware.MinorVersion}");
+            var protocolVersion = session.GetProtocolVersion();
+            Console.WriteLine($"Firmata protocol version {protocolVersion.Major}.{protocolVersion.Minor}");
+
+            session.SetDigitalPinMode(13, PinMode.DigitalOutput);
+            session.SetDigitalPin(13, true);
+            Console.WriteLine("Command sent: LUZ13 on (pin 13)");
+            Console.WriteLine("Press a key");
+            Console.ReadKey(true);
+            session.SetDigitalPin(13, false);
+            Console.WriteLine("Command sent: LUZ13 off");
+        }
+    }
+}
